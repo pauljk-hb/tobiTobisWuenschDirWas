b1af9dab57103e2ed8981da668409eb8a0a5ec29
SimonIT <simonit.orig@gmail.com>
2023-04-23 21:16:34 +0200

    style: Some basic style with sass

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const datastore = require('nedb');
+const sassMiddleware = require('node-sass-middleware')
 
 const app = express();
 app.listen(3000, () => console.log('Turbo Tobi läuft auf Port 3000'))
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}))
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: __dirname + '/public',
+    debug: true,
+    outputStyle: 'compressed'
+}));
 
 const datbase = new datastore('database.db');
 datbase.loadDatabase();
