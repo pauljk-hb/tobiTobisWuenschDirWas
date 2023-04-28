5784563b57fc73bf39e09f45ba992db8a044d418
Paul Kamp <pauljonaskamp@gmx.de>
2023-04-28 17:13:46 +0200

    deploy adjustments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const datastore = require('nedb');
 const sassMiddleware = require('node-sass-middleware')
 
 const app = express();
-app.listen(3000, () => console.log('Turbo Tobi läuft auf Port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Turbo Tobi läuft auf Port 3000'))
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}))
 app.use(sassMiddleware({
